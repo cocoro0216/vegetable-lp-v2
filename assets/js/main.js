d9b0111f9d540616a51df840e0fdffe8e6f7dc33
cocoro0216 <cocoronnnnny@gmail.com>
2026-08-02 16:17:56 +0900

    cssとjsの修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,151 +7,102 @@ gsap.registerPlugin(ScrollTrigger)
 
 
 // // MV
-// gsap.from(".mv__catchCopy--sp", {
-//   opacity: 0,
-//   y: 30,
-//   duration: 1,
-//   ease: "power2.out"
+ gsap.from(".mv__catchCopy--sp", {
+  opacity: 0,
+   y: 30,
+   duration: 1,
+   ease: "power2.out"
 
-// });
+ });
+ gsap.from(".mv__catchCopy--pc", {
+  opacity: 0,
+   y: 30,
+   duration: 1,
+   ease: "power2.out"
 
-// gsap.from(".mv__txt--sp", {
-//   opacity: 0,
-//   y: 30,
-//   duration: 1,
-//   delay: 0.3
+ });
 
-// });
-
-// CTA
-
-// gsap.from(".mv__btn--sp", {
-//   scrollTrigger: ".mv__btn--sp" ,
-//   opacity: 0,
-//   y: 40,
-//   duration: 0.8,
-//   ease: "power2.out"
-
-// });
-// gsap.from(".mv__btn--pc", {
-//   scrollTrigger: ".mv__btn--sp" ,
-//   opacity: 0,
-//   y: 40,
-//   duration: 0.8,
-//   ease: "power2.out"
-
-// });
 
 // // problem
-// gsap.from(".problem", {
-//   scrollTrigger: ".problem" , 
-//   opacity: 0,
-//   y: 40,
-//   duration: 0.8,
-//   ease: "power2.out"
+ gsap.from(".problem", {
+   scrollTrigger: ".problem" , 
+   opacity: 0,
+   y: 40,
+   duration: 0.8,
+   ease: "power2.out"
 
-// });
+ });
 
-// gsap.from(".problem__item", {
-//   scrollTrigger:".problem", 
-//   opacity: 0,
-//   y: 30,
-//   duration: 0.6,
-//   delay: 0.5,
-//   stagger: 0.2,
-//   ease: "power2.out"
+ gsap.from(".problem__item", {
+   scrollTrigger:".problem", 
+   opacity: 0,
+   y: 30,
+   duration: 0.6,
+   delay: 0.5,
+   stagger: 0.2,
+   ease: "power2.out"
 
-// });
+ });
 
 // // lead
-// gsap.from(".lead__img", {
-//   scrollTrigger: ".lead" , 
-//   opacity: 0,
-//   y: 40,
-//   duration: 0.8,
-//   ease: "power2.out"
+ gsap.from(".lead__img", {
+   scrollTrigger: ".lead" , 
+   opacity: 0,
+   y: 40,
+   duration: 0.8,
+   ease: "power2.out"
 
-// });
+ });
 
-// gsap.from(".lead__content", {
-//   scrollTrigger:".lead", 
-//   opacity: 0,
-//   y: 30,
-//   duration: 0.8,
-//   delay: 0.4,
-//   ease: "power2.out"
+ gsap.from(".lead__content", {
+   scrollTrigger:".lead", 
+   opacity: 0,
+   y: 30,
+   duration: 0.8,
+   delay: 0.4,
+   ease: "power2.out"
 
-// });
+ });
 
 // // reason
-// gsap.from(".reason", {
-//   scrollTrigger: ".reason" , 
-//   opacity: 0,
-//   y: 40,
-//   duration: 0.8,
-//   ease: "power2.out"
+ gsap.from(".reason", {
+   scrollTrigger: ".reason" , 
+   opacity: 0,
+   y: 40,
+   duration: 0.8,
+   ease: "power2.out"
 
-// });
+ });
 
-// gsap.from(".reason__item", {
-//   scrollTrigger:".reason", 
-//   opacity: 0,
-//   y: 30,
-//   duration: 0.6,
-//   delay: 0.3,
-//   stagger: 0.2,
-//   ease: "power2.out"
+ gsap.from(".reason__item", {
+   scrollTrigger:".reason", 
+   opacity: 0,
+   y: 30,
+   duration: 0.6,
+   delay: 0.3,
+   stagger: 0.2,
+   ease: "power2.out"
 
-// });
+ });
 // // quality
-// gsap.from(".quality", {
-//   scrollTrigger: ".quality" , 
-//   opacity: 0,
-//   y: 40,
-//   duration: 0.8,
-//   ease: "power2.out"
+ gsap.from(".quality", {
+   scrollTrigger: ".quality" , 
+   opacity: 0,
+   y: 40,
+   duration: 0.8,
+   ease: "power2.out"
 
-// });
-
-// // farmer
-// gsap.from(".farmers", {
-//   scrollTrigger: ".farmers" , 
-//   opacity: 0,
-//   y: 40,
-//   duration: 0.8,
-//   ease: "power2.out"
-
-// });
-
-// gsap.from(".farmers__img", {
-//   scrollTrigger:".farmers", 
-//   opacity: 0,
-//   y: 40,
-//   duration: 0.8,
-//   delay: 0.3,
-//   ease: "power2.out"
-
-// });
-
-// gsap.from(".farmers__content", {
-//   scrollTrigger:".farmers", 
-//   opacity: 0,
-//   y: 30,
-//   duration: 0.8,
-//   delay: 0.6,
-//   ease: "power2.out"
-
-// });
+ });
 
 // // faq
-// gsap.from(".faq", {
-//   scrollTrigger:".faq", 
-//   opacity: 0,
-//   y: 40,
-//   duration: 0.8,
-//   ease: "power2.out"
+ gsap.from(".faq", {
+   scrollTrigger:".faq", 
+   opacity: 0,
+   y: 40,
+   duration: 0.8,
+   ease: "power2.out"
 
-// });
+ });
 
 // =================================================
 // jquery
@@ -171,21 +122,21 @@ gsap.registerPlugin(ScrollTrigger)
 
    
   
-//     $(function(){
-//   $('.faq__item .faq__question').click(function(){
-//     $(this).next('.faq__answer').slideToggle();
-//     $(this).toggleClass("open");
-//   });
-// });
+    $(function(){
+  $('.faq__item .faq__question').click(function(){
+     $(this).next('.faq__answer').slideToggle();
+    $(this).toggleClass("open");
+  });
+ });
 
-//   $('.faq__answer .faq__icon').click(function(){
-//     $(this).closest('.faq__answer').slideUp();
+   $('.faq__answer .faq__icon').click(function(){
+    $(this).closest('.faq__answer').slideUp();
 
-//     $(this)
-//     .closest(".faq__item")
-//     .find('.faq__question')
-//     .removeClass('open');
-//   });
+    $(this)
+     .closest(".faq__item")
+   .find('.faq__question')
+    .removeClass('open');
+  });
 
 
 
